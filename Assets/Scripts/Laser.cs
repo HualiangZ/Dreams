1a4e13f4244a0c5b79030d5d324697d1d15d5989
HualiangZ <zhualiang88@gmail.com>
2024-05-03 12:36:52 +0100

    added laser and laser collider

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public LayerMask toHit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = transform.eulerAngles.z * Mathf.Deg2Rad;
+        Vector2 dir = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, 100f, toHit);
+        if(!hit)
+        {
+            transform.localScale = new Vector3(100f, 0.1f, 1f);
+            return;
+        }
+        transform.localScale = new Vector3(hit.distance, 0.1f, 1f);
+        Debug.Log(hit.collider.gameObject.name);
+        if(hit.collider.tag == "Player")
+        {
+            Destroy(hit.collider.gameObject);
+        }
+    }
+}
